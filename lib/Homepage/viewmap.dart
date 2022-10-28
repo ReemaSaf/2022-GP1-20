0732678b2421bf947c485e9bb4b0c34bb1fc8b96
RazanAldhafian <98522684+RazanAldhafian@users.noreply.github.com>
2022-10-28 19:18:53 +0300

    View Map (Update) Red, Orange and Purple Lines

--- lib/Homepage/viewmap.dart
+++ lib/Homepage/viewmap.dart
@@ -17,7 +17,7 @@ const LatLng MOE = LatLng(24.740716559819983, 46.694773729870136);
 const LatLng Ar_Rabi = LatLng(24.78629195996191, 46.66013792585841);
 const LatLng King_Abdullah_Financial_District =
     LatLng(24.767467334723097, 46.64305311230236);
-const LatLng Yarmuk = LatLng(24.791197738712103, 46.76626014236253);
+const LatLng AlYarmuk = LatLng(24.791197738712103, 46.76626014236253);
 const LatLng Qasr_AlHukm = LatLng(24.62898633421732, 46.71614447119793);
 const LatLng AlWurud = LatLng(24.728513713582622, 46.66647663657381);
 const LatLng STC = LatLng(24.72780279168026, 46.66402971583957);
@@ -25,12 +25,13 @@ const LatLng An_Nuzhah = LatLng(24.748232849680114, 46.712524046659894);
 const LatLng Ad_Douh = LatLng(24.582687500762187, 46.58829571230235);
 const LatLng DK = LatLng(24.724035388519273, 46.655157716642734);
 const LatLng DK2 = LatLng(24.722173718445912, 46.861130749011366);
-const LatLng AlRajhi_Grand_Mosque =
+const LatLng AlRajih_Grand_Mosque =
     LatLng(24.680257767875453, 46.779516666456296);
-const LatLng Othman_Bin_Affan = LatLng(24.801506858888473, 46.69584175608972);
+const LatLng Uthman_bin_Affan_Road =
+    LatLng(24.801506858888473, 46.69584175608972);
 const LatLng King_Salman_Oasis = LatLng(24.717206999063556, 46.638721784925124);
 const LatLng Tuwaiq = LatLng(24.585613556916503, 46.559645359137875);
-const LatLng Garanada_Mall = LatLng(24.786596609860684, 46.72914975780418);
+const LatLng Granada = LatLng(24.786596609860684, 46.72914975780418);
 const LatLng King_Abdulaziz_Road =
     LatLng(24.736628468553576, 46.68478428663959);
 const LatLng Harun_AlRasheed = LatLng(24.686316094328742, 46.796254400133380);
@@ -47,10 +48,11 @@ const LatLng AlSahafah = LatLng(24.81208798511673, 46.62570426013289);
 const LatLng Khalid_Ibn_AlWaleed_Road =
     LatLng(24.768429239510585, 46.75873393175022);
 const LatLng National_Museum = LatLng(24.646564430167828, 46.714283855042105);
-const LatLng DK4 = LatLng(24.60728494286818, 46.654080077775085);
+const LatLng Dhahrat_AlBadiah = LatLng(24.606826444552556, 46.65386732824994);
+//const LatLng DK4 = LatLng(24.60728494286818, 46.654080077775085);
 const LatLng Passport_Department = LatLng(24.6603442332999, 46.70463920106698);
-const LatLng AlNaseem = LatLng(24.701727520269028, 46.82987104286268);
-const LatLng DK5 = LatLng(24.722596428762152, 46.86107112752105);
+const LatLng An_Naseem = LatLng(24.701727520269028, 46.82987104286268);
+const LatLng Khashm_AlAn = LatLng(24.722596428762152, 46.86107112752105);
 const LatLng Sultanah = LatLng(24.61531601628477, 46.68652440053348);
 const LatLng AlDar_AlBaidha = LatLng(24.56012323575007, 46.77638578519186);
 const LatLng First_Industrial_City =
@@ -63,16 +65,27 @@ const AlKhaleej = LatLng(24.782133310064207, 46.79386540053349);
 const LatLng DK7 = LatLng(24.60102299420869, 46.643912831216724);
 const LatLng Southern_Ring_Road = LatLng(24.5988007096572, 46.74559604286267);
 const LatLng SAPTCO = LatLng(24.595952176815352, 46.74780618295626);
-const LatLng Jarir = LatLng(24.675992338837617, 46.760139091132075);
-const LatLng DK8 = LatLng(24.591708184829702, 46.54333827358718);
+const LatLng Jarir_District = LatLng(24.675992338837617, 46.760139091132075);
+const LatLng Jeddah_Road = LatLng(24.591708184829702, 46.54333827358718);
 const LatLng Skirinah = LatLng(24.618221717661566, 46.72486107306263);
 const LatLng Manfouhah = LatLng(24.610701124160094, 46.72741364259594);
 const LatLng AlMargab = LatLng(24.634223633427975, 46.72564952910215);
 const LatLng AlJaradiyah = LatLng(24.62211956062091, 46.697905033439625);
-const LatLng King_Fahad_Stadium = LatLng(24.793133797228098, 46.81058598096131);
+const LatLng King_Fahad_Stadium = LatLng(24.79354574574914, 46.83634875673867);
 const LatLng AlNahdha = LatLng(24.757033610827573, 46.790348717811625);
 const LatLng AlTakhasusi = LatLng(24.724264557913013, 46.65516659868294);
 const LatLng City_Center = LatLng(24.792331161852665, 46.81067411777644);
+const LatLng Aisha_bint_Abi_Bakr_Street =
+    LatLng(24.600585732347877, 46.6438288400605);
+const LatLng Riyadh_Exhibition_Center =
+    LatLng(24.754324431106845, 46.726419308444186);
+const LatLng Railway = LatLng(24.651181683217757, 46.74060586934959);
+const LatLng AlSalihiyah = LatLng(24.6371922661316, 46.7310748022035);
+const LatLng KACT = LatLng(24.718496228417234, 46.64255624108287);
+const LatLng Courts_Complex = LatLng(24.626383321200905, 46.71147031596318);
+const LatLng AlAndalus = LatLng(24.7597732531332, 46.790691465801785);
+const LatLng As_Salam = LatLng(24.723618363309235, 46.810809833504834);
+const LatLng SABIC = LatLng(24.808213877339156, 46.711586173743825);
 
 class ViewMap extends StatefulWidget {
   const ViewMap({Key? key}) : super(key: key);
@@ -126,18 +139,18 @@ class _ViewMap extends State<ViewMap> {
           addMarker('Ar Rabi', Ar_Rabi);
           addMarker('King Abdullah Financial District',
               King_Abdullah_Financial_District);
-          addMarker('Yarmuk', Yarmuk);
+          addMarker('AlYarmuk', AlYarmuk);
           addMarker('Qasr AlHukm', Qasr_AlHukm);
           addMarker('STC', STC);
           addMarker('An Nuzhah', An_Nuzhah);
           addMarker('Ad Douh', Ad_Douh);
           addMarker('DK', DK);
           addMarker('DK2', DK2);
-          addMarker('AlRajhi_Grand_Mosque', AlRajhi_Grand_Mosque);
-          addMarker('Othman Bin Affan', Othman_Bin_Affan);
+          addMarker('AlRajih Grand Mosque', AlRajih_Grand_Mosque);
+          addMarker('Uthman bin Affan Road', Uthman_bin_Affan_Road);
           addMarker('King Salman Oasis', King_Salman_Oasis);
           addMarker('Tuwaiq', Tuwaiq);
-          addMarker('Garanada_Mall', Garanada_Mall);
+          addMarker('Granada', Granada);
           addMarker('King_Abdulaziz_Road', King_Abdulaziz_Road);
           addMarker('Harun_AlRasheed', Harun_AlRasheed);
           addMarker('King_Abdullah_Road', King_Abdullah_Road);
@@ -151,13 +164,13 @@ class _ViewMap extends State<ViewMap> {
           addMarker('AlSahafah', AlSahafah);
           addMarker('Khalid Ibn AlWaleed Road', Khalid_Ibn_AlWaleed_Road);
           addMarker('National_Museum', National_Museum);
-          addMarker('DK4', DK4);
+          // addMarker('DK4', DK4);
           addMarker('Passport_Department', Passport_Department);
-          addMarker('AlNaseem', AlNaseem);
-          addMarker('DK5', DK5);
+          addMarker('AlNaseem', An_Naseem);
+          addMarker('Khashm AlAn', Khashm_AlAn);
           addMarker('Sultanah', Sultanah);
           addMarker('AlDar_AlBaidha', AlDar_AlBaidha);
-          addMarker('First_Industrial_City', First_Industrial_City);
+          addMarker('First Industrial City', First_Industrial_City);
           addMarker('DK6', DK6);
           addMarker('AlHilla', AlHilla);
           addMarker('Aziziyah', Aziziyah);
@@ -165,8 +178,8 @@ class _ViewMap extends State<ViewMap> {
           addMarker('AlKhaleej', AlKhaleej);
           addMarker('SAPTCO', SAPTCO);
           addMarker('Southern_Ring_Road', Southern_Ring_Road);
-          addMarker('Jarir', Jarir);
-          addMarker('DK8', DK8);
+          addMarker('Jarir District', Jarir_District);
+          addMarker('Jeddah Road', Jeddah_Road);
           addMarker('Skirinah', Skirinah);
           addMarker('Manfouhah', Manfouhah);
           addMarker('AlMargab', AlMargab);
@@ -176,6 +189,16 @@ class _ViewMap extends State<ViewMap> {
           addMarker('AlTakhasusi', AlTakhasusi);
           addMarker('City Center Ishniliyah', City_Center);
           addMarker('AlWurud', AlWurud);
+          addMarker('Dhahrat AlBadiah', Dhahrat_AlBadiah);
+          addMarker('Aisha bint Abi Bakr Street', Aisha_bint_Abi_Bakr_Street);
+          addMarker('Riyadh Exhibition Center', Riyadh_Exhibition_Center);
+          addMarker('Railway', Railway);
+          addMarker('AlSalihiyah', AlSalihiyah);
+          addMarker('KACT', KACT);
+          addMarker('Courts Complex', Courts_Complex);
+          addMarker('AlAndalus', AlAndalus);
+          addMarker('As Salam', As_Salam);
+          addMarker('SABIC', SABIC);
         },
         polylines: _polyline,
         markers: _marker.values.toSet(),
@@ -191,14 +214,55 @@ class _ViewMap extends State<ViewMap> {
     City_Center,
     AlKhaleej,
     Al_Hamra,
+    Khalid_Ibn_AlWaleed_Road,
+    Riyadh_Exhibition_Center,
     An_Nuzhah,
     MOE,
     King_Abdulaziz_Road,
     AlWurud,
     STC,
     AlTakhasusi,
+    KACT,
     King_Salman_Oasis,
     King_Saud_University,
+  ];
+
+  //orange line
+  List<LatLng> orange_line = [
+    Khashm_AlAn,
+    Hassan_Ibn_Tahit_Street,
+    An_Naseem,
+    Harun_AlRasheed,
+    AlRajih_Grand_Mosque,
+    Jarir_District,
+    Railway,
+    First_Industrial_City,
+    AlSalihiyah,
+    AlMargab,
+    AlHilla,
+    Qasr_AlHukm,
+    Courts_Complex,
+    AlJaradiyah,
+    Sultanah,
+    Dhahrat_AlBadiah,
+    West_Station,
+    Ad_Douh,
+    Tuwaiq,
+    Jeddah_Road
+  ];
+
+  List<LatLng> purple_line = [
+    King_Abdullah_Financial_District,
+    Ar_Rabi,
+    Uthman_bin_Affan_Road,
+    SABIC,
+    Granada,
+    AlYarmuk,
+    Al_Hamra,
+    AlAndalus,
+    Khurais_Road,
+    As_Salam,
+    An_Naseem,
   ];
 
   addMarker(String id, LatLng location) async {
@@ -220,6 +284,16 @@ class _ViewMap extends State<ViewMap> {
       Polyline(
           polylineId: PolylineId('1'), points: red_line, color: Colors.red),
     );
+
+    _polyline.add(Polyline(
+        polylineId: PolylineId('2'),
+        points: orange_line,
+        color: Colors.orange));
+
+    _polyline.add(Polyline(
+        polylineId: PolylineId('3'),
+        points: purple_line,
+        color: Colors.purple));
 
     _marker[id] = marker;
     setState(() {});
