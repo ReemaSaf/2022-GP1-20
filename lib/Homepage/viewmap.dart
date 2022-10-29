28933a360b9208314e2a623d2fe5e8e46e86dfbc
Yaraalmanea <yaraalmanea@hotmail.com>
2022-10-29 06:34:47 +0300

    add Blue Line

--- lib/Homepage/viewmap.dart
+++ lib/Homepage/viewmap.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names, non_constant_identifier_names
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -17,7 +19,7 @@ const LatLng King_Abdullah_Financial_District =
     LatLng(24.767467334723097, 46.64305311230236);
 const LatLng AlYarmuk = LatLng(24.791197738712103, 46.76626014236253);
 const LatLng Qasr_AlHukm = LatLng(24.629451072535158, 46.716342959436645);
-const LatLng AlWurud = LatLng(24.728513713582622, 46.66647663657381);
+const LatLng AlWurud_1 = LatLng(24.728513713582622, 46.66647663657381);
 const LatLng STC = LatLng(24.72780279168026, 46.66402971583957);
 const LatLng An_Nuzhah = LatLng(24.748232849680114, 46.712524046659894);
 const LatLng Ad_Douh = LatLng(24.582687500762187, 46.58829571230235);
@@ -34,7 +36,7 @@ const LatLng King_Abdulaziz_Road =
     LatLng(24.736628468553576, 46.68478428663959);
 const LatLng Harun_AlRasheed = LatLng(24.686316094328742, 46.796254400133380);
 const LatLng King_Abdullah_Road = LatLng(24.717141214300362, 46.63876470013338);
-const LatLng AlImam_Hospital = LatLng(24.603644941107355, 46.73439692896878);
+const LatLng Al_Iman_Hospital = LatLng(24.60184187076821, 46.73518533497859);
 const LatLng King_Khalid_Airport_Terminal_1_2 =
     LatLng(24.96141030360308, 46.69874671804878);
 const LatLng King_Khalid_Airport_Terminal_3_4 =
@@ -42,30 +44,30 @@ const LatLng King_Khalid_Airport_Terminal_3_4 =
 const LatLng Hassan_Ibn_Tahit_Street =
     LatLng(24.713138180079678, 46.84747031001042);
 const LatLng West_Station = LatLng(24.582103836933722, 46.61461664702924);
-const LatLng AlSahafah = LatLng(24.81208798511673, 46.62570426013289);
+//const LatLng AlSahafah = LatLng(24.81208798511673, 46.62570426013289);
 const LatLng Khalid_Ibn_AlWaleed_Road =
     LatLng(24.768429239510585, 46.75873393175022);
 const LatLng National_Museum = LatLng(24.646564430167828, 46.714283855042105);
 const LatLng Dhahrat_AlBadiah = LatLng(24.606826444552556, 46.65386732824994);
 //const LatLng DK4 = LatLng(24.60728494286818, 46.654080077775085);
-const LatLng Passport_Department = LatLng(24.6603442332999, 46.70463920106698);
+//const LatLng Passport_Department = LatLng(24.6603442332999, 46.70463920106698);
 const LatLng An_Naseem = LatLng(24.701727520269028, 46.82987104286268);
 const LatLng Khashm_AlAn = LatLng(24.722596428762152, 46.86107112752105);
 const LatLng Sultanah = LatLng(24.61531601628477, 46.68652440053348);
-const LatLng AlDar_AlBaidha = LatLng(24.56012323575007, 46.77638578519186);
+const LatLng Ad_Dar_Al_Baida = LatLng(24.56012323575007, 46.77638578519186);
 const LatLng First_Industrial_City =
     LatLng(24.645128413131246, 46.73924760053349);
 const AlHilla = LatLng(24.63236794377303, 46.72130899552275);
-const Aziziyah = LatLng(24.587767444827744, 46.76085043121673);
+const Al_Aziziyah = LatLng(24.587767444827744, 46.76085043121673);
 const Officers_Club = LatLng(24.698023843619875, 46.7180571582043);
 const AlKhaleej = LatLng(24.782133310064207, 46.79386540053349);
 const LatLng DK7 = LatLng(24.60102299420869, 46.643912831216724);
 const LatLng Southern_Ring_Road = LatLng(24.5988007096572, 46.74559604286267);
-const LatLng SAPTCO = LatLng(24.595952176815352, 46.74780618295626);
+//const LatLng SAPTCO = LatLng(24.595952176815352, 46.74780618295626);
 const LatLng Jarir_District = LatLng(24.675992338837617, 46.760139091132075);
 const LatLng Jeddah_Road = LatLng(24.591708184829702, 46.54333827358718);
 const LatLng Skirinah = LatLng(24.618221717661566, 46.72486107306263);
-const LatLng Manfouhah = LatLng(24.610701124160094, 46.72741364259594);
+const LatLng Manfouhah = LatLng(24.610584059288378, 46.7274623001361);
 const LatLng AlMargab = LatLng(24.634223633427975, 46.72564952910215);
 const LatLng AlJaradiyah = LatLng(24.62211956062091, 46.697905033439625);
 const LatLng King_Fahad_Stadium = LatLng(24.79354574574914, 46.83634875673867);
@@ -92,6 +94,27 @@ const LatLng GOSI = LatLng(24.6865786216132, 46.718205888877705);
 const LatLng Ministry_Of_Finance = LatLng(24.65372808690647, 46.71666113915717);
 const LatLng AlSulimaniyah = LatLng(24.711752875638634, 46.70299012147462);
 const LatLng Salahaddin = LatLng(24.7286941806714, 46.70017119397027);
+const LatLng SABB = LatLng(24.82972077842592, 46.61659757976025);
+const LatLng Dr_Sulaiman_Al_Habib =
+    LatLng(24.811755538683347, 46.62551655092465);
+const LatLng Al_Murooj = LatLng(24.75496848415641, 46.65434907660031);
+const LatLng King_Fahad_D1 = LatLng(24.743008722899518, 46.66048326442123);
+const LatLng King_Fahad_D2 = LatLng(24.737262838662325, 46.66339794924775);
+const LatLng AlWurud_2 = LatLng(24.72714294406711, 46.667006085064294);
+const LatLng Al_Urubah = LatLng(24.7145469586332, 46.675103705439156);
+const LatLng Al_Inma_Bank = LatLng(24.704026220178918, 46.68021955092464);
+const LatLng Al_Bilad_Bank = LatLng(24.696571105638657, 46.6837677588918);
+const LatLng King_Fahad_Library = LatLng(24.6870528853412, 46.68858271533797);
+const LatLng Ministry_Of_Interior =
+    LatLng(24.671327695272506, 46.6967090731435);
+const LatLng Al_Murabba = LatLng(24.666771317504566, 46.70160881517249);
+const LatLng Passport_Department =
+    LatLng(24.659545877535543, 46.704598556025594);
+const LatLng Al_Washim = LatLng(24.653022166628176, 46.710859005680014);
+const LatLng Al_Batha = LatLng(24.643876424804503, 46.715439435634565);
+const LatLng Al_Owd = LatLng(24.625852448951708, 46.72142817991293);
+const LatLng Transportation_Center =
+    LatLng(24.598995244455036, 46.74507358152383);
 
 class ViewMap extends StatefulWidget {
   const ViewMap({Key? key}) : super(key: key);
@@ -157,33 +180,33 @@ class _ViewMap extends State<ViewMap> {
           addMarker('King Salman Oasis', King_Salman_Oasis);
           addMarker('Tuwaiq', Tuwaiq);
           addMarker('Granada', Granada);
-          addMarker('King_Abdulaziz_Road', King_Abdulaziz_Road);
-          addMarker('Harun_AlRasheed', Harun_AlRasheed);
-          addMarker('King_Abdullah_Road', King_Abdullah_Road);
-          addMarker('AlImam_Hospital', AlImam_Hospital);
-          addMarker('King_Khalid_Airport_Terminal_1_2',
+          addMarker('King Abdulaziz Road', King_Abdulaziz_Road);
+          addMarker('Harun AlRasheed', Harun_AlRasheed);
+          addMarker('King Abdullah Road', King_Abdullah_Road);
+          addMarker('Al Iman Hospital', Al_Iman_Hospital);
+          addMarker('King Khalid Airport terminal 1,2',
               King_Khalid_Airport_Terminal_1_2);
-          addMarker('King_Khalid_Airport_Terminal_3_4',
+          addMarker('King Khalid Airport Terminal 3,4',
               King_Khalid_Airport_Terminal_3_4);
-          addMarker('Hassan_Ibn_Tahit_Street', Hassan_Ibn_Tahit_Street);
+          addMarker('Hassan Ibn Tahit Street', Hassan_Ibn_Tahit_Street);
           addMarker('West_Station', West_Station);
-          addMarker('AlSahafah', AlSahafah);
+          // addMarker('AlSahafah', AlSahafah);
           addMarker('Khalid Ibn AlWaleed Road', Khalid_Ibn_AlWaleed_Road);
-          addMarker('National_Museum', National_Museum);
+          addMarker('National Museum', National_Museum);
           // addMarker('DK4', DK4);
-          addMarker('Passport_Department', Passport_Department);
+          addMarker('Passport Department', Passport_Department);
           addMarker('AlNaseem', An_Naseem);
           addMarker('Khashm AlAn', Khashm_AlAn);
           addMarker('Sultanah', Sultanah);
-          addMarker('AlDar_AlBaidha', AlDar_AlBaidha);
+          addMarker('AlDar AlBaidha', Ad_Dar_Al_Baida);
           addMarker('First Industrial City', First_Industrial_City);
           addMarker('AlSulimaniyah', AlSulimaniyah);
           addMarker('AlHilla', AlHilla);
-          addMarker('Aziziyah', Aziziyah);
-          addMarker('Officers_Club', Officers_Club);
+          addMarker('Aziziyah', Al_Aziziyah);
+          addMarker('Officers Club', Officers_Club);
           addMarker('AlKhaleej', AlKhaleej);
-          addMarker('SAPTCO', SAPTCO);
-          addMarker('Southern_Ring_Road', Southern_Ring_Road);
+          //addMarker('SAPTCO', SAPTCO);
+          addMarker('Southern Ring Road', Southern_Ring_Road);
           addMarker('Jarir District', Jarir_District);
           addMarker('Jeddah Road', Jeddah_Road);
           addMarker('Skirinah', Skirinah);
@@ -194,7 +217,7 @@ class _ViewMap extends State<ViewMap> {
           addMarker('AlNahdha', AlNahdha); //53
           addMarker('AlTakhasusi', AlTakhasusi);
           addMarker('City Center Ishniliyah', City_Center);
-          addMarker('AlWurud', AlWurud);
+          addMarker('AlWurud 1', AlWurud_1);
           addMarker('Dhahrat AlBadiah', Dhahrat_AlBadiah);
           addMarker('Aisha bint Abi Bakr Street', Aisha_bint_Abi_Bakr_Street);
           addMarker('Riyadh Exhibition Center', Riyadh_Exhibition_Center);
@@ -206,13 +229,28 @@ class _ViewMap extends State<ViewMap> {
           addMarker('As Salam', As_Salam);
           addMarker('SABIC', SABIC);
           addMarker('Abu Dhabi Square', Abu_Dhabi_Square);
-          addMarker('Ad_Dhabab', Ad_Dhabab);
+          addMarker('Ad Dhabab', Ad_Dhabab);
           addMarker('GOSI', GOSI);
           addMarker('AlWizarat', AlWizarat);
           addMarker('MEW & A', Mew_and_A); //5A2
           addMarker('Ministry Of Defence', Ministry_Of_Defence); //defence
           addMarker('Ministry Of Finance', Ministry_Of_Finance);
           addMarker('Salahaddin', Salahaddin);
+          addMarker('SABB', SABB);
+          addMarker('Dr Sulaiman Al Habib', Dr_Sulaiman_Al_Habib);
+          addMarker('Al Murooj', Al_Murooj);
+          addMarker('King Fahad District 1', King_Fahad_D1);
+          addMarker('King Fahad District 2', King_Fahad_D2);
+          addMarker('AlWurud 2', AlWurud_2);
+          addMarker('Al Urubah', Al_Urubah);
+          addMarker('Al Inma Bank', Al_Inma_Bank);
+          addMarker('Al Bilad Bank', Al_Bilad_Bank);
+          addMarker('King Fahad Library', King_Fahad_Library);
+          addMarker('Ministry Of Interior', Ministry_Of_Interior);
+          addMarker('Al Murabba', Al_Murabba);
+          addMarker('Passport Department', Passport_Department);
+          addMarker('Al Washim', Al_Washim),
+          addMarker('Al Owd', Al_Owd);
         },
         polylines: _polyline,
         markers: _marker.values.toSet(),
@@ -233,7 +271,7 @@ class _ViewMap extends State<ViewMap> {
     An_Nuzhah,
     MOE,
     King_Abdulaziz_Road,
-    AlWurud,
+    AlWurud_1,
     STC,
     AlTakhasusi,
     KACT,
@@ -294,6 +332,35 @@ class _ViewMap extends State<ViewMap> {
     National_Museum,
   ];
 
+  List<LatLng> blue_line = [
+    SABB,
+    Dr_Sulaiman_Al_Habib,
+    King_Abdullah_Financial_District,
+    Al_Murooj,
+    King_Fahad_D1,
+    King_Fahad_D2,
+    STC,
+    AlWurud_2,
+    Al_Urubah,
+    Al_Inma_Bank,
+    Al_Bilad_Bank,
+    King_Fahad_Library,
+    Ministry_Of_Interior,
+    Al_Murabba,
+    Passport_Department,
+    Al_Washim,
+    National_Museum,
+    Al_Batha,
+    Qasr_AlHukm,
+    Al_Owd,
+    Skirinah,
+    Manfouhah,
+    Al_Iman_Hospital,
+    Transportation_Center,
+    Al_Aziziyah,
+    Ad_Dar_Al_Baida
+  ];
+
   addMarker(String id, LatLng location) async {
     var markerIcon = await BitmapDescriptor.fromAssetImage(
       const ImageConfiguration(),
@@ -315,17 +382,24 @@ class _ViewMap extends State<ViewMap> {
     );
 
     _polyline.add(Polyline(
-        polylineId: PolylineId('2'),
+        polylineId: const PolylineId('2'),
         points: orange_line,
         color: Colors.orange));
 
     _polyline.add(Polyline(
-        polylineId: PolylineId('3'),
+        polylineId: const PolylineId('3'),
         points: purple_line,
         color: Colors.purple));
 
     _polyline.add(Polyline(
-        polylineId: PolylineId('4'), points: green_line, color: Colors.green));
+        polylineId: const PolylineId('4'),
+        points: green_line,
+        color: Colors.green));
+
+    _polyline.add(Polyline(
+        polylineId: const PolylineId('5'),
+        points: blue_line,
+        color: Colors.blue));
 
     _marker[id] = marker;
     setState(() {});
@@ -424,4 +498,3 @@ class _ViewMap extends State<ViewMap> {
 //         });
 //   }
 // }
-
