5396ec538b79cd00cf5305a6f3674aa2b2f74788
ReemaSaf <98449142+ReemaSaf@users.noreply.github.com>
2022-11-02 01:36:14 +0300

    Locate device location Permissions

---
+++ lib/Homepage/services/locationServices.dart
@@ -0,0 +1,23 @@
+import 'package:permission_handler/permission_handler.dart';
+
+import '../../helpers/helper_fucntions.dart';
+
+class LocationServices {
+  Future<bool> checkForPermission(context) async {
+    var status = await Permission.location.request();
+    if (status.isGranted) {
+      return true;
+    } else if (status.isDenied) {
+      showSnackBarMessage(context: context, content: "Permission is denied,");
+      return false;
+    } else if (status.isLimited) {
+      return true;
+    } else if (status.isPermanentlyDenied) {
+      await openAppSettings();
+      var status2 = await Permission.location.request();
+      return status2.isGranted ? true : false;
+    } else {
+      return false;
+    }
+  }
+}
---
+++ lib/helpers/helper_fucntions.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+dp({msg, arg}) => debugPrint("\n $msg =$arg \n ");
+
+showSnackBarMessage({required BuildContext context, required String content}) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7)),
+      backgroundColor: Colors.white,
+      content: Text(
+        content,
+        style: TextStyle(
+          color: Colors.black,
+
+        ),
+        overflow: TextOverflow.visible,
+      )));
+}
