a2769e3094991ea0e0a635d67713688639efe555
najjd <95283422+najjd@users.noreply.github.com>
2022-11-05 01:44:06 +0300

    merging

--- lib/helpers/station_controller.dart
+++ lib/helpers/station_controller.dart
@@ -1,9 +1,15 @@
 // ignore_for_file: non_constant_identifier_names, avoid_print
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:sekkah_app/helpers/DistanceModel.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import '../Homepage/providers/locationProvier.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'bus_station_model.dart';
+
+import '../core/const.dart';
+import 'metro_station_model.dart';
 
 class StationsController extends GetxController {
   final provider = Get.put(LocationProvider());
@@ -41,5 +47,33 @@ class StationsController extends GetxController {
     update();
   }
 
-  get_Metro_stations() async {}
+  get_Metro_stations() async {
+       stations_loading(true);
+
+       final position = provider.currentLatLang;
+
+       var result =
+           await FirebaseFirestore.instance.collection("Metro_Station").get();
+       printInfo(info: '$result');
+
+       final data = result.docs.map((e) => MetroStationModel.fromMap(e.data())).toList();
+       final tempList = <DistanceModel>[];
+       for (var item in data) {
+         final location = item.Location;
+         await provider.getRoutDistance(location, position).then((distance) {
+           tempList.add(DistanceModel.mapToMetroModel(item, distance));
+         });
+       }
+       BusStations.addAll(tempList);
+       BusStations.sort(
+         (a, b) {
+           return a.Distance.compareTo(b.Distance);
+         },
+       );
+
+       print("here is result data ${result.docs.length}");
+
+       stations_loading(false);
+       update();
+  }
 }
