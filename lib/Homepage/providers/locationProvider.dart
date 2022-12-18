7d9db46f87242bea0f57b45e7d10cb82952c2085
najjd <95283422+najjd@users.noreply.github.com>
2022-12-18 23:41:12 +0300

    location

--- lib/Homepage/providers/locationProvider.dart
+++ lib/Homepage/providers/locationProvider.dart
@@ -1,11 +1,8 @@
 // ignore_for_file: file_names, avoid_function_literals_in_foreach_calls, avoid_print
 
 import 'dart:async';
-import 'dart:math';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
@@ -47,68 +44,25 @@ class LocationProvider extends ChangeNotifier {
   }
 
   Future<void> animateToLocation(LatLng location) async {
+    final GoogleMapController controller = await completer.future;
     dp(msg: "Camera animate");
-    await googleMapController!.animateCamera(CameraUpdate.newCameraPosition(
+    await controller.animateCamera(CameraUpdate.newCameraPosition(
         CameraPosition(
             bearing: 192.8334901395799, target: location, zoom: 14)));
   }
 
-
   Future<double> distanceBetween(
       GeoPoint endLocation, LatLng? currentPosition) async {
-   
-   final  double distance = Geolocator.distanceBetween(endLocation.latitude, endLocation.longitude, 
-           currentPosition!.latitude, currentPosition.longitude)  / 1000 ; 
+    final double distance = Geolocator.distanceBetween(
+            endLocation.latitude,
+            endLocation.longitude,
+            currentPosition!.latitude,
+            currentPosition.longitude) /
+        1000;
 
-   print(distance);
+    print(distance);
 
-return distance;
-
-
-
-  /*double calculateDistance(lat1, lon1, lat2, lon2) {
-    var p = 0.017453292519943295;
-    var a = 0.5 -
-        cos((lat2 - lat1) * p) / 2 +
-        cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2;
-    return 12742 * asin(sqrt(a));
-  }
-
-  Future<double> getRoutDistance(
-      GeoPoint endLocation, LatLng? currentPosition) async {
-    double distance = 0.0;
-    PolylinePoints polylinePoints = PolylinePoints();
-    List<LatLng> polylineCoordinates = [];
-
-    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
-      Const.apiKey,
-      PointLatLng(currentPosition!.latitude, currentPosition.longitude),
-      PointLatLng(endLocation.latitude, endLocation.longitude),
-      travelMode: TravelMode.driving,
-    );
-
-    if (result.points.isNotEmpty) {
-      result.points.forEach((PointLatLng point) {
-        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
-      });
-    } else {
-      print(result.errorMessage);
-    }
-
-    //polyline Coordinates is the List of longitude and latidtude.
-    double totalDistance = 0;
-    for (var i = 0; i < polylineCoordinates.length - 1; i++) {
-      totalDistance += calculateDistance(
-          polylineCoordinates[i].latitude,
-          polylineCoordinates[i].longitude,
-          polylineCoordinates[i + 1].latitude,
-          polylineCoordinates[i + 1].longitude);
-    }
-    print('distance : $totalDistance');
-
-    distance = totalDistance;
-    return distance;*/
-
+    return distance;
 
   }
 }
